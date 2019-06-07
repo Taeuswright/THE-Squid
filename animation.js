0ba0b0fc41c90c996302d147e1c1b697a9ca65c2
Taeus Wright <taeus@pecsafety.com>
2019-06-07 08:48:32 -0500

    made a js file

---
+++ animation.js
@@ -0,0 +1,27 @@
+
+function bigImg(x) {
+  x.style.height = "25%";
+  x.style.width = "15%";
+}
+    
+function normalImg(x) {
+  x.style.height = "25%";
+  x.style.width = "18%";
+}       
+      
+
+          function myMove() {
+            var elem = document.getElementById("ink");   
+            var pos = 300;
+            var id = setInterval(frame, 5);
+            function frame() {
+            if (pos == 1550) {
+              clearInterval(id);
+          } else {
+              pos++; 
+              elem.style.right = pos + 'px'; 
+              elem.style.left = pos + 'pos'; 
+          
+              } 
+            }
+          }        
