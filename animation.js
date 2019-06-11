4033df4ef60bc0bf7ef477515653167eb9531f17
Taeus Wright <taeus@pecsafety.com>
2019-06-11 13:25:14 -0500

    cleaned up

--- animation.js
+++ animation.js
@@ -1,27 +1,24 @@
-
 function bigImg(x) {
   x.style.height = "25%";
   x.style.width = "15%";
 }
-    
+
 function normalImg(x) {
   x.style.height = "25%";
   x.style.width = "18%";
-}       
-      
+}
 
-          function myMove() {
-            var elem = document.getElementById("ink");   
-            var pos = 300;
-            var id = setInterval(frame, 5);
-            function frame() {
-            if (pos == 1550) {
-              clearInterval(id);
-          } else {
-              pos++; 
-              elem.style.right = pos + 'px'; 
-              elem.style.left = pos + 'pos'; 
-          
-              } 
-            }
-          }        
+function myMove() {
+  var elem = document.getElementById("ink");
+  var pos = 300;
+  var id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 1550) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.right = pos + "px";
+      elem.style.left = pos + "pos";
+    }
+  }
+}
